96b40b4b5aec86be5e6c05ea1ca425bc3ac23339
Guido Mantegna <Guido>
2025-06-16 16:21:40 -0300

    First version of home animations

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,57 +6,44 @@ import {
   useScroll,
 } from "motion/react";
 
-const draw = {
-  hidden: { pathLength: 0, opacity: 0 },
-  visible: (i: number) => {
-    const delay = 1 + i * 0.1;
-    return {
-      // dashArray: 200,
-      // pathLength: 1,
-      opacity: 1,
-      transition: {
-        // pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
-        opacity: { delay, duration: 0.01 },
-      },
-    };
-  },
-};
+// const draw = {
+//   hidden: { pathLength: 0, opacity: 0 },
+//   visible: (i: number) => {
+//     const delay = 1 + i * 0.1;
+//     return {
+//       // dashArray: 200,
+//       // pathLength: 1,
+//       opacity: 1,
+//       transition: {
+//         // pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
+//         opacity: { delay, duration: 0.01 },
+//       },
+//     };
+//   },
+// };
 
-const frontend: { [key: number]: string } = {
-  0: "Front-end Developer",
-  1: "Software Engineer",
-  2: "Technology Enthusiast",
-  3: "",
-};
+const frontend = [
+  "<Front-end Developer/>",
+  "<Software Engineer/>",
+  "<JS Developer/>",
+];
 
 const FrontendVariants = (isScr0llingUp: boolean) => ({
   initial: {
     opacity: 0,
     y: isScr0llingUp ? 20 : -20,
     filter: "blur(10px)",
-    // transition: {
-    //   opacity: { duration: 0.5 },
-    //   y: { duration: 0.05 },
-    // },
-    transition: { duration: .5 }
+    transition: { duration: 0.5 },
   },
   animate: {
     opacity: 1,
     y: 0,
     filter: "blur(0px)",
-    // transition: {
-    //   opacity: { duration: 0.5 },
-    //   y: { duration: 0.25 },
-    // },
   },
   exit: {
     opacity: 0,
     y: isScr0llingUp ? -20 : 20,
     filter: "blur(10px)",
-    // transition: {
-    //   opacity: { duration: 0.25 },
-    //   y: { duration: 0.25 },
-    // },
     transition: {
       duration: 0.25,
     },
@@ -65,27 +52,39 @@ const FrontendVariants = (isScr0llingUp: boolean) => ({
 
 const Home: React.FC = () => {
   const [H1BackPosition, setH1BackPosition] = useState({ x: 0, y: 0 });
-  const H1Ref = React.useRef<HTMLHeadingElement>(null);
-  const { scrollYProgress, scrollY } = useScroll();
-  const [scrollYValue, setScrollYValue] = useState(0);
+  const titleRef = React.useRef<HTMLDivElement>(null);
+  const { scrollYProgress, scrollY } = useScroll({
+    target: titleRef,
+    offset: ["start start", "end end"],
+  });
+  const [selectedTextIndex, setSelectedTextIndex] = useState(0);
   const [isScr0llingUp, setIsScr0llingUp] = useState(false);
+  const [h1before, setH1before] = useState(0);
+
   useMotionValueEvent(scrollYProgress, "change", (latest) => {
-    // latest is the up-to-date value
-    // Do something with latest
-    console.log({ latest: Math.floor((latest * 100) / 4) });
-    const frontedMSG = (prevState: number) => {
-      let simplifiedLatest = Math.floor((latest * 100) / 4);
+    // console.log({ latest: Math.floor((latest * 100) / 4) });
+    let simplifiedLatest = Math.floor((latest * 100) / 3);
+
+    const textNumber = (prevState: number) => {
       if (prevState === simplifiedLatest || simplifiedLatest > 3)
         return prevState;
       return simplifiedLatest;
     };
-    setScrollYValue((prevState) => frontedMSG(prevState));
+
+    setSelectedTextIndex(Math.floor(latest * 10));
+    // setSelectedTextIndex((prevState) => textNumber(prevState));
+
     const prev = scrollY.getPrevious();
     const curr = scrollY.get();
     setIsScr0llingUp(
       prev !== undefined && curr !== undefined ? prev > curr : false
     );
-    console.log(isScr0llingUp ? "Scrolling Up" : "Scrolling Down");
+
+    console.log("lastest", -20 * latest * 10 + 200);
+    // console.log("lastest", (-20 * Math.floor(latest *10)) + 200);
+
+    setH1before(-25 * latest * 10 + 200);
+    // setH1before(() => 100 - latest * 10 * 10);
   });
 
   useEffect(() => {
@@ -100,15 +99,19 @@ const Home: React.FC = () => {
       const isUp = y < YCenter;
       const isLeft = x < XCenter;
 
-      setH1BackPosition({ x: isLeft ? -4 : 4, y: isUp ? 1 : -1 });
+      console.log("mouse position", {
+        x: Math.floor((x / windowWidth) * 100),
+        y: Math.floor((y / windowHeight) * 100),
+      });
+      // setH1BackPosition({ x: isLeft ? -4 : 4, y: isUp ? 1 : -1 });
+      setH1BackPosition({
+        x: Math.floor((x / windowWidth) * 100),
+        y: Math.floor((y / windowHeight) * 100),
+      });
     };
 
-    // console.log("scrollYProgress", scrollYProgress.get())
-
-    // Add event listener
     document.addEventListener("mousemove", setH1BG);
 
-    // Remove event listener
     return () => {
       document.removeEventListener("mousemove", setH1BG);
     };
@@ -116,117 +119,58 @@ const Home: React.FC = () => {
 
   return (
     <>
-      <section className="h-[200vh]" id="home">
-        {/* <section className="h-[200vh] flex items-center" id="home"> */}
-
+      <section className="h-[250vh] relative bg-white" id="home" ref={titleRef}>
         <motion.div
-          // initial={{ y: "-20px" }}
-          // whileInView={{ y: "0" }}
-          // transition={{ type: "spring" }}
-          className="sticky top-0"
+          className="sticky top-0 h-[100vh] py-[4%] flex flex-col justify-center h1-main"
+          style={{
+            ["--main-clip" as any]: `circle(5% at ${H1BackPosition.x}% ${H1BackPosition.y}%)`,
+          }}
         >
-          <motion.div style={{ scaleX: scrollYProgress }} />
-          <h1
-            ref={H1Ref}
-            className={`text-[140px] leading-[110px] text-left font-extrabold py-[4%] ps-[5%] h-[100vh] content-center flex flex-col`}
+          <motion.h1
+            className={`text-[140px] leading-[100px] text-left font-extrabold relative z-[1] pl-12 `}
+            layout
+          >
+            Guido
+            <br />
+            Mantegna.
+          </motion.h1>
+
+          <AnimatePresence mode="popLayout">
+            {frontend.map((text, index) => {
+              if (index !== selectedTextIndex) return null;
+              return (
+                <motion.h2
+                  className="text-[75px] text-gray-950 font-bold mt-4 pl-12"
+                  key={`frontend-${index}`}
+                  variants={FrontendVariants(isScr0llingUp)}
+                  initial="initial"
+                  animate="animate"
+                  exit="exit"
+                >
+                  {text}
+                </motion.h2>
+              );
+            })}
+          </AnimatePresence>
+          <motion.div
+            className="absolute w-full h-full left-0 py-[4%] flex flex-col justify-center h1-mask"
             style={{
-              backgroundPositionX: `${H1BackPosition.x}%`,
-              backgroundPositionY: `${H1BackPosition.y}%`,
+              // clipPath: `inset(0% 0px 0px 0px)`,
+              clipPath: `inset(${h1before}% 0px 0px 0px)`,
+              ["--after-top" as any]: `${h1before}%`,
             }}
           >
-            {/* Guido Mantegna. */}
-            <motion.span>Guido</motion.span>
-            <AnimatePresence mode="popLayout">
-              {/* {scrollYValue < 0.04 && ( */}
-              <motion.div>
-                <motion.span
-                  initial={{
-                    opacity: 0,
-                    // scale: 0,
-                    // y: -20,
-                  }}
-                  animate={{
-                    opacity: 1,
-                    // scale: 1,
-                    // y: 0,
-                  }}
-                  exit={{
-                    opacity: 0,
-                    // scale: 0,
-                    // y: -20,
-                  }}
-                  transition={{
-                    duration: 0.25,
-                    // ease: "backInOut",
-                    // % of time for each keyframe to be reached
-                    // times: [0, 0.25, 0.5, 0.85, 1],
-                  }}
-                >
-                  Mantegna.
-                </motion.span>
-              </motion.div>
-              {/* )} */}
-              <motion.span className="text-[60px] text-gray-950 flex">
-                <AnimatePresence mode="popLayout">
-                  {"<"}
-
-                  {/* {frontend[scrollYValue].split("").map((el, i) => ( */}
-                  <motion.p
-                  // key={i}
-                  // variants={draw}
-                  // custom={i}
-                  // initial="hidden"
-                  // animate="visible"
-                  // className={el === "d" ? "pr-[15px]" : ""}
-                  >
-                    {/* {el} */}
-                  </motion.p>
-                  {/* ))} */}
-                  {/* <p className="relative">{" />"}</p> */}
-                </AnimatePresence>
-              </motion.span>
-              <AnimatePresence mode="popLayout">
-                {scrollYValue === 0 && (
-                  <motion.span
-                    className="text-[60px] text-gray-950 flex"
-                    key="frontend-0"
-                    variants={FrontendVariants(isScr0llingUp)}
-                    initial="initial"
-                    animate="animate"
-                    exit="exit"
-                  >
-                    {frontend[scrollYValue]}
-                  </motion.span>
-                )}
-                {scrollYValue === 1 && (
-                  <motion.span
-                    className="text-[60px] text-gray-950 flex"
-                    key="frontend-1"
-                    variants={FrontendVariants(isScr0llingUp)}
-                    initial="initial"
-                    animate="animate"
-                    exit="exit"
-                  >
-                    {frontend[scrollYValue]}
-                  </motion.span>
-                )}
-                {scrollYValue === 2 && (
-                  <motion.span
-                    className="text-[60px] text-gray-950 flex"
-                    key="frontend-2"
-                    variants={FrontendVariants(isScr0llingUp)}
-                    initial="initial"
-                    animate="animate"
-                    exit="exit"
-                  >
-                    {frontend[scrollYValue]}
-                  </motion.span>
-                )}
-              </AnimatePresence>
-            </AnimatePresence>
-          </h1>
+            <motion.h1
+              className={`text-[140px] leading-[100px] text-left font-extrabold relative z-[1] h1-text pl-12 `}
+            >
+              Guido
+              <br />
+              Mantegna.
+            </motion.h1>
+          </motion.div>
         </motion.div>
       </section>
+      {/* <div className="h-screen"></div> */}
     </>
   );
 };
