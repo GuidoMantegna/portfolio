5ac0eabb40cb0d9bdeb3fd46f42b98ce2568ee5b
Guido Mantegna <Guido>
2025-09-02 17:21:17 -0300

    Modify 'About' layout

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,27 +1,29 @@
-import Avatar from "../assets/images/avatar.png"
+import Avatar from "../assets/images/avatar.png";
 
 const About: React.FC = () => {
   return (
-    <section
-      className="h-screen flex items-center bg-[#1386EB] text-white p-8"
-      id="about"
-    >
-      <div className="mx-16">
-        <h2 className="font-extralight text-2xl">About me</h2>
-        <h3 className="font-black text-6xl mt-3">Hi! I'm</h3>
-        <h3 className="font-black text-6xl -mt-3">Guido Mantegna</h3>
+    <section className="h-[200vh] flex items-center text-white p-8" id="about">
+      <div className="mx-[10%]">
+        <div className="flex justify-between items-center">
+          <div>
+            <h2 className="font-extralight text-2xl">About me</h2>
+            <h3 className="font-black text-6xl mt-3">Hi! I'm</h3>
+            <h3 className="font-black text-6xl -mt-3">Guido Mantegna</h3>
+          </div>
 
-        <div className="mt-8">
-          <p className="font-extralight text-sm leading-9">
+          <img alt="GM Pic" src={Avatar} className="h-[140px]" />
+        </div>
+        <div className="mt-12 font-extralight text-sm leading-7 text-justify">
+          <p className="">
             <span className="font-medium">Senior Frontend Developer</span> with
             a thorough understanding of
-            <span className="font-medium">React</span> and its core principles,
+            <span className="font-medium"> React</span> and its core principles,
             experienced in its most popular workflows, design patterns and
             libraries. Proficiency in developing
-            <span className="font-medium">end-to-end</span> applications using
+            <span className="font-medium"> end-to-end</span> applications using
             the <span className="font-medium">MERN</span> stack.
           </p>
-          <p className="font-extralight text-sm mt-2 leading-9">
+          <p className="mt-4">
             Driven by a passion for{" "}
             <span className="font-medium">
               building intuitive and high-quality applications
@@ -36,13 +38,8 @@ const About: React.FC = () => {
           </p>
         </div>
       </div>
-      <img
-        alt="GM Pic"
-        src={Avatar}
-        className="w-[210px] self-baseline mt-24 mr-[5%]"
-      />
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
