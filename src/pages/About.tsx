efca8830cbd129a04ed5a4893451403aa92711ba
Guido Mantegna <Guido>
2025-09-11 07:27:58 -0300

    Add marquee with skills/stack within About section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,17 +1,61 @@
+import React, { useLayoutEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 import Avatar from "../assets/images/avatar.png";
 
-const About: React.FC = () => {
-  return (
-    <section className="h-[200vh] flex items-center text-white p-8" id="about">
-      <div className="mx-[10%]">
-        <div className="flex justify-between items-center">
-          <div>
-            <h2 className="font-extralight text-2xl">About me</h2>
-            <h3 className="font-black text-6xl mt-3">Hi! I'm</h3>
-            <h3 className="font-black text-6xl -mt-3">Guido Mantegna</h3>
-          </div>
+const HARD_SKILLS = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React.js",
+  "Tailwind",
+  "Git",
+  "Node",
+  "MongoDB",
+  "Express",
+  "Next.js",
+  "Jest",
+  "SASS",
+  "Figma",
+  "Chakra UI",
+  "Jira",
+  "Postman",
+  "SCRUM",
+  "NPM",
+];
 
-          <img alt="GM Pic" src={Avatar} className="h-[140px]" />
+const About: React.FC = () => {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [contentWidth, setContentWidth] = useState(0);
+
+  useLayoutEffect(() => {
+    if (contentRef.current) {
+      setContentWidth(contentRef.current.scrollWidth);
+    }
+  }, []);
+  return (
+    <motion.section
+      id="about"
+      className="h-[200vh] flex items-center justify-center text-white"
+    >
+      <motion.div
+        className="mx-[10%] w-3/4"
+        initial={{ y: 100, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        // viewport={{ amount: "all", once: true }}
+        transition={{ delay: 0.5, duration: 0.5 }}
+      >
+        <div className="flex gap-8 items-center">
+          <div className="">
+            <h2 className="font-extralight text-2xl">About me</h2>
+            <h3 className="font-black text-6xl mt-3">
+              Hi! I'm <br />
+              Guido Mantegna
+            </h3>
+          </div>
+          <div className="flex-1">
+            <img alt="GM Pic" src={Avatar} className="max-w-[230px] m-auto" />
+          </div>
         </div>
         <div className="mt-12 font-extralight text-sm leading-7 text-justify">
           <p className="">
@@ -37,8 +81,51 @@ const About: React.FC = () => {
             cultivated interpersonal and communication abilities.
           </p>
         </div>
-      </div>
-    </section>
+        {/* <motion.div className="overflow-hidden w-full mt-8">
+          <motion.ul
+            className="flex gap-4 text-xl text-nowrap"
+            initial={{ x: 0 }}
+            animate={{ x: "-100%" }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "loop",
+              ease: "linear",
+            }}
+          >
+            {HARD_SKILLS.map((skill) => (
+              <motion.li
+                key={skill}
+              >
+                {skill}
+              </motion.li>
+            ))}
+          </motion.ul>
+        </motion.div> */}
+        <div className="overflow-hidden w-full mt-8 relative marquee">
+          <motion.div
+            className="flex whitespace-nowrap"
+            animate={{ x: [-0, -contentWidth] }}
+            transition={{
+              duration: 30,
+              repeat: Infinity,
+              ease: "linear",
+            }}
+          >
+            {/* Duplicate the list twice for seamless loop */}
+            {[...Array(2)].map((_, i) => (
+              <div ref={contentRef} className=" ">
+                <ul key={i} className="flex gap-8 text-xl mr-8">
+                  {HARD_SKILLS.map((skill) => (
+                    <li key={skill}>{skill}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </motion.div>
+        </div>
+      </motion.div>
+    </motion.section>
   );
 };
 
